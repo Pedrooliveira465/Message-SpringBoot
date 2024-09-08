b352ddafc780ab384a3c3b3aa7adf2608e0bd33a
Pedro Oliveira <pedro86gugalegal@gmail.com>
2024-09-08 20:00:23 -0300

    Project created

---
+++ src/main/java/br/com/pedrooliveira/message_springboot/MessageSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrooliveira.message_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSpringbootApplication.class, args);
+	}
+
+}
