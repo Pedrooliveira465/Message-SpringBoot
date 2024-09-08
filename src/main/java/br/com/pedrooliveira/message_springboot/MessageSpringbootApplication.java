5c49ac736e02cd98395a999f003842c4e3fc3a20
Pedro Oliveira <pedro86gugalegal@gmail.com>
2024-09-08 20:41:52 -0300

    Created Kafka

--- src/main/java/br/com/pedrooliveira/message_springboot/MessageSpringbootApplication.java
+++ src/main/java/br/com/pedrooliveira/message_springboot/MessageSpringbootApplication.java
@@ -2,6 +2,11 @@ package br.com.pedrooliveira.message_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @SpringBootApplication
 public class MessageSpringbootApplication {
@@ -10,4 +15,20 @@ public class MessageSpringbootApplication {
 		SpringApplication.run(MessageSpringbootApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/kafka")
+class HelloController {
+	private HelloProducer service;
+
+	public HelloController(HelloProducer service) {
+		this.service = service;
+	}
+
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		service.sendMessage("Hello, " + name);
+		return "OK";
+	}
 }
